ba998836a079027f656cbc94df307255f58d3f57
Greg Ejimofor <grgonyx@gmail.com>
2023-04-19 07:53:13 -0700

    commit tree e5b52b0fed2699f65cec44c53cc05c79fe600189

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names = ["Dave", "Frank", "Chris"];
+const event = "Wedding";
+
+function writeCards(names, event){
+    let messages = [];
+    for(let i = 0; i < names.length; i++ ){
+        let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+
+let cards = writeCards(names, event);
+console.log(cards);
+
+function countDown(){
+    let i = 10;
+    while (i >= 0){
+      console.log(i);
+      i--
+    }
+  }
+  countDown();
